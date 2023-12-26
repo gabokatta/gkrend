4c2a31b4f884988476c224bcec6fa31e65ee7767
gabokatta <gkatta@fi.uba.ar>
2023-12-26 06:01:28 -0300

    Add: Camera Modes

---
+++ src/engine/cameras/camera.ts
@@ -0,0 +1,12 @@
+import type { mat4, vec3 } from "gl-matrix";
+import type { WebGL } from "../webgl";
+
+export interface Camera {
+
+
+    update(gl: WebGL): void;
+    getViewMatrix(): mat4;
+    lookAt(position: vec3): void;
+    clean(): void;
+    
+}
---
+++ src/engine/cameras/state.ts
@@ -0,0 +1,38 @@
+
+export interface CameraState {
+    initial(): CameraState;
+}
+
+export class OrbitalState implements CameraState {
+
+    x: number = 0;
+    y: number = 0;
+    z: number = 0;
+    u: number = 0;
+    v: number = 0;
+    du: number = 0;
+    dv: number = 0;
+    dz: number = 0;
+
+
+    initial(): CameraState {
+        return new OrbitalState();
+    }
+    
+}
+
+export class DroneState implements CameraState {
+
+    fwd: number = 0;
+    right: number = 0;
+    u: number = 0;
+    v: number = 0;
+    du: number = 0;
+    dv: number = 0;
+
+
+    initial(): CameraState {
+        return new DroneState();
+    }
+    
+}
