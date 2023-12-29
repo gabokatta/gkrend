cac04e91dd1d56cc5dcc406d5b07202052e37104
gabokatta <gkatta@fi.uba.ar>
2023-12-29 18:41:44 -0300

    Add: Animation Module.

--- src/rendering/animation.ts
+++ src/rendering/animation.ts
@@ -1,4 +1,5 @@
 import { Object3D, Transformation } from "../engine/object";
+import { ApplicableAnimation } from "./properties";
 
 
 export interface Animation {
@@ -11,7 +12,7 @@ export class Rotate implements Animation {
 
     public axis: number[] = [1,0,1]
 
-    applyFrame(object: Object3D, t: number): void {
+    public applyFrame(object: Object3D, t: number): void {
         object.updateTransform([
             Transformation.rotation(t, this.axis)
         ]);
@@ -23,20 +24,31 @@ export class Resize implements Animation {
 
     private flip: boolean = false;
 
-    applyFrame(object: Object3D, t: number): void {
+    public applyFrame(object: Object3D, t: number): void {
 
         var scaleFactor :number;
-        const mod = (n: number) => { return Number((n).toFixed(2)) % 1.5; }
+        var modFactor: number = 1.5;
+        const mod = (n: number) => { return Number((n).toFixed(2)) % modFactor; }
 
         if (mod(t) == 0) {
             this.flip = !this.flip;
         }
 
-        scaleFactor =  this.flip ? mod(t) : 1.5 - mod(t);
+        scaleFactor =  this.flip ? mod(t) : modFactor - mod(t);
 
         object.updateTransform([
             Transformation.scale([scaleFactor,scaleFactor,scaleFactor])
         ])
     }
     
+}
+
+export const RESIZE: Animation = new Resize();
+export const ROTATE: Animation = new Rotate();
+export function applyAnimations(animations: ApplicableAnimation[], object: Object3D, tick: number) {
+    animations.forEach( (applicable) => {
+        if (applicable.enabled) {
+            applicable.animation.applyFrame(object, tick);
+        } 
+    })
 }
--- src/rendering/properties.ts
+++ src/rendering/properties.ts
@@ -1,3 +1,54 @@
-export var sceneProperties = {
+import { DrawMethod } from "../engine/webgl";
+import { Animation } from "./animation";
+
+
+export class SceneProperties {
+
+    public shape: Shapes = Shapes.SPHERE;
+    public texture: Textures = Textures.NONE;
+    public camera: Cameras = Cameras.ORBITAL;
+    public render: DrawMethod = DrawMethod.Smooth;
+
+    public animations: ApplicableAnimation[] = []
+    public velocity: number = 2;
     
+    public sphereRadius: number = 3;
+    public coneRadius: number = 2;
+    public coneHeight: number = 4;
+    public cubeWidth: number = 2;
+    public cubeHeight: number = 2;
+    public cubeDepth: number = 2;
+    public cylinderHeight: number = 4;
+    public cylinderRadius: number = 2;
+
+
+}
+
+export class ApplicableAnimation {
+    public animation: Animation;
+    public enabled: boolean = false;
+
+    constructor(animation: Animation, enabled: boolean) {
+        this.animation = animation;
+        this.enabled = enabled;
+    }
+}
+
+export enum Cameras {
+    DRONE,
+    ORBITAL
+}
+
+export enum Textures {
+    NONE,
+    LAVA,
+    WATER,
+    GRASS
+}
+
+export enum Shapes {
+    CONE,
+    CYLINDER,
+    SPHERE,
+    CUBE
 }
