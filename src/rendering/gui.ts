6d418ab06487546c57edb06a9ba97af04e6040c4
gabokatta <gkatta@fi.uba.ar>
2024-01-03 13:28:18 -0300

    Add: Full GUI support

--- src/rendering/gui.ts
+++ src/rendering/gui.ts
@@ -1,5 +1,96 @@
 import { GUI } from "dat.gui";
+import { Shapes, props } from "./properties";
 
 
-var gui = new GUI({ autoPlace:false, width:200 });
+var gui = new GUI({ autoPlace:false, width:225 });
 document.querySelector('.gui-container')?.append(gui.domElement);
+
+var colorFolder: GUI = gui.addFolder("Color");
+colorFolder.addColor(props, 'shapeColor')
+
+var sizingFolder: GUI = gui.addFolder("Sizing")
+var sphereMenu = buildSphereMenu(sizingFolder);
+var coneMenu = buildConeMenu(sizingFolder);
+var cubeMenu = buildCubeMenu(sizingFolder);
+var cylinderMenu = buildCylinderMenu(sizingFolder);
+var sizeChildren = [coneMenu, cubeMenu, sphereMenu, cylinderMenu]
+
+var animationFolder: GUI = gui.addFolder("Animation")
+animationFolder.add(props, 'rotspeed', 1, 10, 1).step(1)
+animationFolder.add(props, 'scalespeed', 1, 10).step(1)
+animationFolder.add(props, 'rotate').onChange((value) => { props.rotate = value;})
+animationFolder.add(props, 'scale').onChange((value) => { props.scale = value;})
+animationFolder.open()
+
+hideFolders(sizeChildren)
+colorFolder.open()
+sizingFolder.open()
+sphereMenu.show()
+sphereMenu.open()
+
+
+function buildSphereMenu(gui: GUI): GUI {
+    var sphereMenu = gui.addFolder('Sphere')
+    sphereMenu.add(props, 'sphereRadius', 1, 5, 1)
+    return sphereMenu
+}
+
+function buildConeMenu(gui: GUI): GUI {
+    var coneMenu = gui.addFolder('Cone')
+    coneMenu.add(props, 'coneRadius', 1, 5, 1)
+    coneMenu.add(props, 'coneHeight', 1, 5, 1)
+    return coneMenu
+}
+
+function buildCylinderMenu(gui: GUI): GUI {
+    var cylinderMenu = gui.addFolder('Cylinder')
+    cylinderMenu.add(props, 'cylinderRadius', 1, 5, 1)
+    cylinderMenu.add(props, 'cylinderHeight', 1, 5, 1)
+    return cylinderMenu
+}
+
+function buildCubeMenu(gui: GUI): GUI {
+    var cubeMenu = gui.addFolder('Cube')
+    cubeMenu.add(props, 'cubeDepth', 1, 5, 1)
+    cubeMenu.add(props, 'cubeHeight', 1, 5, 1)
+    cubeMenu.add(props, 'cubeWidth', 1, 5, 1)
+    return cubeMenu
+}
+
+
+const shapeButtons = document.querySelectorAll('.shape-btn');
+shapeButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) =>  renderShapeMenu(e));
+})
+
+
+function renderShapeMenu(e: Event) {
+    const pressedButton = e.target as HTMLElement;
+    hideFolders(sizeChildren);
+    switch (pressedButton.id) {
+        case Shapes.SPHERE: {
+            sphereMenu.show()
+            sphereMenu.open()
+            break
+        }
+        case Shapes.CONE: {
+            coneMenu.show()
+            coneMenu.open()
+            break
+        }
+        case Shapes.CYLINDER: {
+            cylinderMenu.show()
+            cylinderMenu.open()
+            break
+        }
+        case Shapes.CUBE: {
+            cubeMenu.show()
+            cubeMenu.open()
+            break
+        }
+    }
+}
+
+function hideFolders(folders: GUI[]) {
+    folders.forEach((f) => f.hide())
+}
--- src/rendering/properties.ts
+++ src/rendering/properties.ts
@@ -2,28 +2,6 @@ import { DrawMethod } from "../engine/webgl";
 import { Animation } from "./animation";
 
 
-export class SceneProperties {
-
-    public shape: Shapes = Shapes.SPHERE;
-    public texture: Textures = Textures.NONE;
-    public camera: Cameras = Cameras.ORBITAL;
-    public render: DrawMethod = DrawMethod.Smooth;
-
-    public animations: ApplicableAnimation[] = []
-    public velocity: number = 2;
-    
-    public sphereRadius: number = 3;
-    public coneRadius: number = 2;
-    public coneHeight: number = 4;
-    public cubeWidth: number = 2;
-    public cubeHeight: number = 2;
-    public cubeDepth: number = 2;
-    public cylinderHeight: number = 4;
-    public cylinderRadius: number = 2;
-
-
-}
-
 export class ApplicableAnimation {
     public animation: Animation;
     public enabled: boolean = false;
@@ -47,8 +25,33 @@ export enum Textures {
 }
 
 export enum Shapes {
-    CONE,
-    CYLINDER,
-    SPHERE,
-    CUBE
+    CONE = "cone",
+    CYLINDER = "cylinder",
+    SPHERE = "sphere",
+    CUBE = "cube"
 }
+
+
+export var props = {
+    shape: Shapes.SPHERE,
+    texture: Textures.NONE,
+    camera: Cameras.ORBITAL,
+    render: DrawMethod.Smooth,
+
+    shapeColor: [255, 0, 255],
+
+    animations: [],
+    rotate: false,
+    rotspeed: 2,
+    scale: false,
+    scalespeed: 2,
+    
+    sphereRadius: 3,
+    coneRadius: 2,
+    coneHeight: 4,
+    cubeWidth: 2,
+    cubeHeight: 2,
+    cubeDepth: 2,
+    cylinderHeight: 4,
+    cylinderRadius: 2
+};
